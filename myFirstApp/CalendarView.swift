0415d77f9a891cb4ab52471e98a536525393cb3c
hongssup <hmk0202@naver.com>
2021-01-22 16:21:22 +0900

    Add FSCalendar

--- myFirstApp/CalendarView.swift
+++ myFirstApp/CalendarView.swift
@@ -5,4 +5,29 @@
 //  Created by SeoYeon on 2021/01/22.
 //
 
+import UIKit
+import Foundation
+import FSCalendar
 
+class CalendarView: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
+    
+    fileprivate weak var calendar: FSCalendar!
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .white
+        self.view = view
+        
+        let calendar = FSCalendar(frame: CGRect(x: 20, y: 30, width: 320, height: 300))
+        calendar.dataSource = self
+        calendar.delegate = self
+        view.addSubview(calendar)
+        self.calendar = calendar
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
