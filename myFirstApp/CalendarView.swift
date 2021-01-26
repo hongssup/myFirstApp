77e7b110a5dc736cafbbcafd8f42b839fcc89a05
hongssup <hmk0202@naver.com>
2021-01-26 15:22:30 +0900

    Modify calendar UI

--- myFirstApp/CalendarView.swift
+++ myFirstApp/CalendarView.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Foundation
 import FSCalendar
+import MaterialComponents
 
 class CalendarView: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
     
@@ -18,7 +19,8 @@ class CalendarView: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
         view.backgroundColor = .systemGray6
         self.view = view
         
-        let calendar = FSCalendar(frame: CGRect(x: 20, y: 30, width: 320, height: 300))
+        //let calendar = FSCalendar(frame: CGRect(x: 20, y: 30, width: 320, height: 400))
+        let calendar = FSCalendar()
         calendar.dataSource = self
         calendar.delegate = self
         view.addSubview(calendar)
@@ -28,6 +30,26 @@ class CalendarView: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupCalendar()
+        addConstraints()
     }
     
+    private func setupCalendar() {
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        calendar.appearance.headerTitleColor = MDCPalette.deepOrange.tint500
+        calendar.appearance.todayColor = .none
+        calendar.appearance.titleTodayColor = MDCPalette.deepOrange.tint500
+        calendar.appearance.weekdayTextColor = .darkGray
+        calendar.appearance.titleWeekendColor = MDCPalette.deepOrange.tint800
+    }
+    
+    private func addConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            calendar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
+            calendar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10),
+            calendar.heightAnchor.constraint(equalToConstant: 400)        
+        ])
+    }
 }
